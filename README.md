cfe7ed611faf98c9a168ae000944c4429f969cd5
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2017-07-05 13:01:22 -0700

    checkout and card

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Simple Mobile App using Mastercard Qkr! APIs #
-Uses the [node-qkr-api-server](https://github.com/perusworld/node-qkr-api-server) to call Qkr! APIs. This implementation allows browsing for products(by default the first merchant, first menu, first product variant would be selected), add them to a cart and then checkout.
+Uses the [node-qkr-api-server](https://github.com/perusworld/node-qkr-api-server) to call Qkr! APIs. This implementation allows browsing for products(by default the first merchant, first menu, first product variant, first card would be used), add them to a cart and then checkout.
 
- If you are looking to call Qkr! APIs directly then head over to [node-qkr-api](https://github.com/perusworld/node-qkr-api)
+If you are looking to call Qkr! APIs directly then head over to [node-qkr-api](https://github.com/perusworld/node-qkr-api)
  
 
 ## Run ##
--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 import { QkrServiceProvider } from '../../providers/qkr-service/qkr-service';
+import { ConfigServiceProvider } from '../../providers/config-service/config-service';
 
 import { CartPage } from '../cart/cart';
 
@@ -14,11 +15,14 @@ import { CartPage } from '../cart/cart';
 export class CheckoutPage {
 
   public cart: any = {};
-  constructor(public navCtrl: NavController, public navParams: NavParams, private qkr: QkrServiceProvider, private auth: AuthServiceProvider) {
+  public card: any = {};
+  public paymentEnabled = false;
+  constructor(public navCtrl: NavController, public navParams: NavParams, private qkr: QkrServiceProvider, private auth: AuthServiceProvider, private cfg: ConfigServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CheckoutPage');
+    this.paymentEnabled = false;
     this.loadCart();
   }
 
@@ -26,14 +30,29 @@ export class CheckoutPage {
  * loadCart
  */
   public loadCart() {
-    this.qkr.carts(this.auth.authToken).
-      then(resp => {
-        if (0 < resp.length) {
-          this.cart = resp[0];
-        }
-      }).catch(err => {
-        console.log(err);
+    this.cfg.presentLoading('Loading ...').then(resp => {
+      return this.qkr.carts(this.auth.authToken);
+    }).then(resp => {
+      if (0 < resp.length) {
+        this.cart = resp[0];
+        return this.qkr.cards(this.auth.authToken);
+      } else {
+        throw 'Empty Cart';
+      }
+    }).then(resp => {
+      if (0 < resp.length) {
+        this.card = resp[0];
+        this.paymentEnabled = true;
+        this.cfg.hideLoading();
+      } else {
+        throw 'No Card';
+      }
+    }).catch(err => {
+      console.error(err);
+      this.cfg.hideLoading().then(resp => {
+        this.cfg.showAlert('Cart Load Failed!', JSON.stringify(err, null, 2));
       });
+    });
   }
 
   /**
@@ -47,6 +66,24 @@ export class CheckoutPage {
    * checkout
    */
   public checkout() {
+    this.cfg.presentLoading('Processing ...').then(resp => {
+      return this.qkr.checkout(this.auth.authToken, {
+        amountMinorUnits: this.cart.amountMinorUnits,
+        cardId: this.card.id,
+        cartId: this.cart.cartId,
+        tipAmount: 0
+      });
+    }).then(status => {
+      this.paymentEnabled = false;
+      return this.cfg.showAlert('Checkout completed successfuly!', `${status.thankYouMessage} this is your reference number ${status.ref}`)
+    }).then(status => {
+      this.cfg.hideLoading();
+    }).catch(err => {
+      console.error(err);
+      this.cfg.hideLoading().then(resp => {
+        this.cfg.showAlert('Checkout Failed!', JSON.stringify(err, null, 2));
+      });
+    });
   }
 
 
